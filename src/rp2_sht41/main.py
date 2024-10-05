f2d386cd4753c148a61eb46ce5d3a5d5e9f5a897
rollcloud <rollcloud@gmail.com>
2024-10-05 14:20:26 +0100

    fix: add server integration

--- src/rp2_sht41/main.py
+++ src/rp2_sht41/main.py
@@ -1,6 +1,20 @@
 import time
 from machine import Pin, I2C
 from micropython_sht4x import sht4x
+import urequests as requests
+import network
+
+WIFI_NAME = "Wifi_name"
+WIFI_PASSWORD = "Wifi_password"
+SERVER = "http://localhost:5000"
+
+# Connect to WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(WIFI_NAME, WIFI_PASSWORD)
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("Waiting to connect:")
+    time.sleep(1)
 
 i2c = I2C(0, sda=Pin(4), scl=Pin(5))
 sht = sht4x.SHT4X(i2c)
@@ -9,7 +23,16 @@ sht.temperature_precision = sht4x.LOW_PRECISION
 
 while True:
     temperature, relative_humidity = sht.measurements
-    print(f"Temperature: {temperature:.2f}*C")
-    print(f"Relative Humidity: {relative_humidity:.2f}%")
-    print("")
+    payload = {
+        "Timestamp": time.time(),
+        "Temperature": temperature,
+        "Pressure": None,
+        "Humidity": relative_humidity,
+        "Air Quality": None,
+        "eCO2": None,
+    }
+    headers = {"Content-Type": "application/json"}
+    res = requests.post(url=SERVER + "/data", headers=headers, json=payload)
+
+    print(payload)
     time.sleep(0.5)

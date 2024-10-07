65591dda0fae6a86f6ab70c280435b7350a60da3
rollcloud <rollcloud@gmail.com>
2024-10-07 18:08:44 +0100

    fix: enter deep sleep and measure battery voltage

---
+++ src/rp2_sht41/hardware.py
@@ -0,0 +1,84 @@
+"""Hardware-related functions for the Raspberry Pi Pico."""
+
+from machine import ADC, Pin
+import network
+import time
+
+led = Pin("LED", Pin.OUT)
+
+
+def read_vsys():
+    """
+    Read the system voltage from the ADC, disabling the WLAN chip to avoid interference.
+
+    Source: https://www.reddit.com/r/raspberrypipico/comments/xalach/comment/ipigfzu/
+    """
+    conversion_factor = 3 * 3.3 / 65535
+    wlan = network.WLAN(network.STA_IF)
+    wlan_active = wlan.active()
+
+    try:
+        # Don't use the WLAN chip for a moment.
+        wlan.active(False)
+
+        # Make sure pin 25 is high.
+        Pin(25, mode=Pin.OUT, pull=Pin.PULL_DOWN).high()
+
+        # Reconfigure pin 29 as an input.
+        Pin(29, Pin.IN)
+
+        vsys = ADC(29)
+        return vsys.read_u16() * conversion_factor
+
+    finally:
+        # Restore the pin state and possibly reactivate WLAN
+        Pin(29, Pin.ALT, pull=Pin.PULL_DOWN, alt=7)
+        wlan.active(wlan_active)
+
+
+def connect_to_wifi(wifi_name, wifi_password):
+    """Connect to WiFi network using the given SSID and password.
+
+    Based on the example from the MicroPython documentation, 3.6. Connecting to a wireless network:
+    https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf with LED feedback.
+
+    The function will attempt to connect to the network and will toggle the onboard LED while waiting.
+    If the connection is successful, the LED will turn off and the IP address will be printed.
+    If the connection fails, the LED will turn on and an error will be raised.
+    """
+    global led
+    led.on()
+
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(wifi_name, wifi_password)
+
+    # Wait for connect or fail
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print("waiting for connection...")
+        led.toggle()
+        time.sleep(1)
+
+    # Handle connection error
+    if wlan.status() != 3:
+        led.on()
+        print("network connection failed")
+    else:
+        led.off()
+        print("connected")
+        status = wlan.ifconfig()
+        print("ip = " + status[0])
+
+    return wlan
+
+
+def deactivate_wifi(wlan):
+    """Completely deactivate the WiFi interface."""
+    wlan.disconnect()
+    wlan.active(False)
+    wlan.deinit()
+    print("WiFi deactivated")
--- src/rp2_sht41/main.py
+++ src/rp2_sht41/main.py
@@ -1,9 +1,18 @@
-import time
-from machine import Pin, I2C, unique_id
+"""
+Send temperature, humidity data from a SHT4x sensor to a server.
+
+The server address, WiFi credentials, and update period are defined as constants
+at the beginning of the script and should be changed as required.
+
+After sending the data, the script will enter deep sleep mode for the specified
+period, before waking up and running the main.py script again.
+"""
+
+from machine import Pin, I2C, unique_id, deepsleep
 from micropython_sht4x import sht4x
-import network
 import urequests
 import errno
+from hardware import connect_to_wifi, led, read_vsys, deactivate_wifi
 
 WIFI_NAME = "Wifi_name"
 WIFI_PASSWORD = "Wifi_password"
@@ -13,87 +22,47 @@ UPDATE_PERIOD = 60  # seconds
 # pico-specific hardware-based unique ID
 uid = "".join("{:02x}".format(x) for x in unique_id())
 
-led = Pin("LED", Pin.OUT)
-
-
-def connect_to_wifi(wifi_name, wifi_password):
-    """Connect to WiFi network using the given SSID and password.
-
-    Based on the example from the MicroPython documentation, 3.6. Connecting to a wireless network:
-    https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf with LED feedback.
-
-    The function will attempt to connect to the network and will toggle the onboard LED while waiting.
-    If the connection is successful, the LED will turn off and the IP address will be printed.
-    If the connection fails, the LED will turn on and an error will be raised.
-    """
-    global led
-    led.on()
-
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(wifi_name, wifi_password)
-
-    # Wait for connect or fail
-    max_wait = 10
-    while max_wait > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        max_wait -= 1
-        print("waiting for connection...")
-        led.toggle()
-        time.sleep(1)
-
-    # Handle connection error
-    if wlan.status() != 3:
-        led.on()
-        print("network connection failed")
-    else:
-        led.off()
-        print("connected")
-        status = wlan.ifconfig()
-        print("ip = " + status[0])
-
-    return wlan
-
+voltage = read_vsys()
 
 wlan = connect_to_wifi(WIFI_NAME, WIFI_PASSWORD)
+
 i2c = I2C(0, sda=Pin(4), scl=Pin(5))
 sht = sht4x.SHT4X(i2c)
 sht.temperature_precision = sht4x.LOW_PRECISION
 
-while True:
-    location = 1  # integer from 0 to 8
-    temperature, relative_humidity = sht.measurements
-    temperature = round(temperature, 2)
-    relative_humidity = round(relative_humidity, 2)
+location = 1  # integer from 0 to 8
+temperature, relative_humidity = sht.measurements
+temperature = round(temperature, 2)
+relative_humidity = round(relative_humidity, 2)
 
-    payload = {
-        "UID": uid,
-        "Location": location,
-        "Temperature": temperature,
-        "Humidity": relative_humidity,
-    }
-    headers = {"Content-Type": "application/json"}
-    try:
-        # Send data to the server, flash the LED while waiting
-        led.on()
-        response = urequests.post(url=SERVER + "/data", headers=headers, json=payload)
-        response.close()
-        print("Payload successfully sent")
-    except OSError as e:
-        if e.errno == errno.ECONNABORTED:
-            print("Connection aborted (ECONNABORTED)")
-            print("Server not found")
-        else:
-            print("Error sending payload:", e)
-    except Exception as e:
+payload = {
+    "UID": uid,
+    "Location": location,
+    "Voltage": voltage,
+    "Temperature": temperature,
+    "Humidity": relative_humidity,
+}
+headers = {"Content-Type": "application/json"}
+
+try:
+    # Send data to the server, flash the LED while waiting
+    led.on()
+    response = urequests.post(url=SERVER + "/data", headers=headers, json=payload)
+    response.close()
+    print("Payload successfully sent")
+except OSError as e:
+    if e.errno == errno.ECONNABORTED:
+        print("Connection aborted (ECONNABORTED)")
+        print("Server not found")
+    else:
         print("Error sending payload:", e)
-        # Reconnect to WiFi if the connection is lost
-        print("could not connect (status = " + str(wlan.status()) + ")")
-        if wlan.status() < 0 or wlan.status() >= 3:
-            wlan.disconnect()
-            wlan = connect_to_wifi(WIFI_NAME, WIFI_PASSWORD)
-    finally:
-        led.off()
+except Exception as e:
+    print("Error sending payload:", e)
+    print("could not connect (status = " + str(wlan.status()) + ")")
+finally:
+    led.off()
 
-    time.sleep(UPDATE_PERIOD)
+deactivate_wifi(wlan)
+
+print("Going to sleep for", UPDATE_PERIOD, "seconds")
+deepsleep(UPDATE_PERIOD * 1000)
